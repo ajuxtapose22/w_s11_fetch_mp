17245e7848eb7cea91b77647dbc5ba0e7e1c46ca
ajuxtapose22 <aaron.jardin@yahoo.com>
2024-08-24 14:18:18 -0700

    form completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Routes, Route, NavLink } from 'react-router-dom'
 import DogForm from './DogForm'
 import DogsList from './DogsList'
 
 export default function App() {
+  const [dogs, setDogs] = useState([])
+  const [currentDogId, setCurrentDog] = useState(null)
+
+  useEffect(() => { getDogs()}, [])
+
+  const getDogs = () => {
+    fetch("/api/dogs")
+    .then(response => {
+      if(!response.ok) 
+        throw new Error('Problem GETing the dogs')
+   
+        return response.json()
+    })
+    .then(setDogs)
+    .catch(err => console.error(err))
+  }
   return (
     <div>
       <nav>
@@ -11,8 +27,16 @@ export default function App() {
         <NavLink to="/form">Form</NavLink>
       </nav>
       <Routes>
-        <Route path="/" element={<DogsList />} />
-        <Route path="/form" element={<DogForm />} />
+        <Route path="/" element={<DogsList 
+          dogs={dogs}
+          getDogs={getDogs}
+          setCurrentDog={setCurrentDog}
+        />} />
+        <Route path="/form" element={<DogForm 
+          dog={currentDogId && dogs.find(dog => dog.id == currentDogId)}
+          getDogs={getDogs}
+          reset={() => setCurrentDog(null)}
+        />} />
       </Routes>
     </div>
   )
